0994bb195dd417f788d46caf7cd0903495a0c57c
Camly <camlysales@gmail.com>
2025-09-26 20:49:39 +0000

    contents are overflowing in grid in desktop screen , remove contents and

--- src/components/landing/app-grid.tsx
+++ src/components/landing/app-grid.tsx
@@ -2,7 +2,6 @@ import {
   Card,
   CardHeader,
   CardTitle,
-  CardDescription,
 } from '@/components/ui/card';
 import { AnimateOnScroll } from '@/components/landing/animate-on-scroll';
 import Link from 'next/link';
@@ -27,13 +26,13 @@ export function AppGrid() {
             delay={app.delay}
           >
             <Link href={`/apps/${index}`} className="h-full block group">
-              <Card className="h-full transform transition-transform duration-300 hover:-translate-y-2 hover:shadow-xl lg:h-56 flex flex-col justify-center text-center p-4">
+              <Card className="h-full transform transition-transform duration-300 hover:-translate-y-2 hover:shadow-xl flex flex-col justify-center text-center p-6">
                 <CardHeader>
                   <AnimateOnScroll
                     className="data-[state=closed]:opacity-0 data-[state=open]:animate-in data-[state=open]:fade-in-0 data-[state=open]:zoom-in-95"
                     delay={app.delay + 100}
                   >
-                    <div className="w-16 h-16 mx-auto mb-4 rounded-full flex items-center justify-center animated-icon-background">
+                    <div className="w-20 h-20 mx-auto mb-6 rounded-full flex items-center justify-center animated-icon-background">
                        <div className="w-[calc(100%-4px)] h-[calc(100%-4px)] bg-card rounded-full flex items-center justify-center">
                         {app.icon}
                       </div>
@@ -43,17 +42,9 @@ export function AppGrid() {
                     className="data-[state=closed]:opacity-0 data-[state=open]:animate-in data-[state=open]:fade-in-0 data-[state=open]:slide-in-from-bottom-2"
                     delay={app.delay + 200}
                   >
-                    <CardTitle className="font-headline text-lg">
+                    <CardTitle className="font-headline text-xl">
                       {app.title}
                     </CardTitle>
-                  </AnimateOnScroll>
-                  <AnimateOnScroll
-                    className="data-[state=closed]:opacity-0 data-[state=open]:animate-in data-[state=open]:fade-in-0 data-[state=open]:slide-in-from-bottom-3"
-                    delay={app.delay + 300}
-                  >
-                    <CardDescription className="pt-2 text-xs">
-                      {app.description}
-                    </CardDescription>
                   </AnimateOnScroll>
                 </CardHeader>
               </Card>
